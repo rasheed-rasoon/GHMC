34cc970c70c6781b72831a9d552da4a075a45ad8
Rasheed Ahmed <rasheedsheik66@gmail.com>
2019-02-24 20:48:07 +0530

    my third commit

--- Sports_Inventory/src/test/java/com/cgg/testcases/GHMCGitSample.java
+++ Sports_Inventory/src/test/java/com/cgg/testcases/GHMCGitSample.java
@@ -3,5 +3,5 @@ package com.cgg.testcases;
 public class GHMCGitSample {
 public static void main(String args[])
 {
-	System.out.println("my second build");}
+	System.out.println("my second build was attached");}
 }
