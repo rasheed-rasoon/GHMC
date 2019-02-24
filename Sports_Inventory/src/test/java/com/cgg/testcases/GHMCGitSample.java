fff055ef6880c2fa229dca59f6e7129c3990841b
Rasheed Ahmed <rasheedsheik66@gmail.com>
2019-02-24 21:02:26 +0530

    sixth commit

--- Sports_Inventory/src/test/java/com/cgg/testcases/GHMCGitSample.java
+++ Sports_Inventory/src/test/java/com/cgg/testcases/GHMCGitSample.java
@@ -5,5 +5,6 @@ public static void main(String args[])
 {
 	System.out.println("my second build was attached");
 	System.out.println("my fifth build was attached");
+	System.out.println("my six build was attached");
 }
 }
