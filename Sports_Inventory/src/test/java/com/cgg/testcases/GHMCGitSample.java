3d11983645b293b9adf25712e8c4774d9a4b6672
Rasheed Ahmed <rasheedsheik66@gmail.com>
2019-02-24 20:54:43 +0530

    my fifth commit

--- Sports_Inventory/src/test/java/com/cgg/testcases/GHMCGitSample.java
+++ Sports_Inventory/src/test/java/com/cgg/testcases/GHMCGitSample.java
@@ -3,5 +3,7 @@ package com.cgg.testcases;
 public class GHMCGitSample {
 public static void main(String args[])
 {
-	System.out.println("my second build was attached");}
+	System.out.println("my second build was attached");
+	System.out.println("my fifth build was attached");
+}
 }
